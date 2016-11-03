357b22128310a6db3704db97baf10bbb62550a80
Zeng zhining <zengzhining@lai-you.net>
2016-11-03 18:39:06 +0800

    buildAndroid脚本

--- buildAndroid.sh
+++ buildAndroid.sh
@@ -1,7 +1,8 @@
 #/sbin/sh
-git pull origin master 
+#git pull origin master 
 cocos compile -p android --ap android-19 --ndk-toolchain     arm-linux-androideabi-4.8
 cd ./simulator/android
+adb uninstall com.sadi.spaceShooter
 adb install piexPlane-debug.apk
 
 
