3f4b6c228b8e72881ccc5784c36d9d7cbe0b4c11
zengzhining <272682545@qq.com>
2016-11-14 21:07:54 +0800

    增加结算界面

---
+++ src/app/views/SelectScene.lua
@@ -0,0 +1,107 @@
+local SelectScene = class("SelectScene", cc.load("mvc").ViewBase)
+
+SelectScene.RESOURCE_FILENAME = "Layer/SelectRole.csb"
+
+local TAG_ROLE_1 = 101
+local TAG_ROLE_2 = 102
+
+function SelectScene:onCreate()
+	self.roleId_ = 1
+	local root = self:getResourceNode()
+
+	--title
+	local title = root:getChildByName("Des")
+	self.title_ = title
+	--button
+	local startBtn = root:getChildByName("Go")
+	startBtn:onTouch(function ( event )
+		if event.name == "ended" then
+			__G__MenuCancelSound()
+			self:getApp():enterLoading("GameScene")
+		end
+	end)
+	--left
+	local leftBtn = root:getChildByName("Left")
+	self.leftBtn_ = leftBtn
+	leftBtn:onTouch(function ( event )
+		if event.name == "ended" then
+			__G__MenuCancelSound()
+			self:onLeft()
+		end
+	end)
+
+	local rightBtn = root:getChildByName("Right")
+	self.rightBtn_ = rightBtn
+	rightBtn:onTouch(function ( event )
+		if event.name == "ended" then
+			__G__MenuCancelSound()
+			self:onRight()
+		end
+	end)
+
+	if self.roleId_ == 1 then 
+		leftBtn:hide()
+	elseif self.roleId_ == 2 then 
+		rightBtn:hide()
+	end
+
+	self:updateTitle(self.roleId_)
+
+	self:createRole(1)
+	self:createRole(2)
+
+	self:updateRole()
+end
+
+function SelectScene:createRole(id_)
+	local role = PlaneFactory:getInstance():createRole(id_)
+	role:pos(display.cx, display.cy)
+	if id_ == 1 then
+		self:addChild(role,100, TAG_ROLE_1)
+	elseif id_ == 2 then 
+		self:addChild(role,100, TAG_ROLE_2)
+	end
+end
+
+function SelectScene:updateRole()
+
+	local role = PlaneFactory:getInstance():createRole(self.roleId_)
+	role:pos(display.cx, display.cy)
+	self:addChild(role,100, TAG_ROLE)
+end
+
+function SelectScene:onLeft(  )
+	-- body
+	self.roleId_ = 1
+	self.leftBtn_:hide()
+	self.rightBtn_:show()
+	self:updateTitle(self.roleId_)
+end
+
+function SelectScene:onRight(  )
+	-- body
+	self.roleId_ = 2
+	self.rightBtn_:hide()
+	self.leftBtn_:show()
+	self:updateTitle(self.roleId_)
+
+end
+
+function SelectScene:updateTitle( id )
+	local str = nil
+	if id == 1 then 
+		str = "Faster,Less Bullet"
+	elseif id == 2 then 
+		str = "Slower, More Bullet"
+	end
+	self.title_:setString(str)
+end
+function SelectScene:onEnter()
+
+end
+
+function SelectScene:onExit()
+
+end
+
+return SelectScene
