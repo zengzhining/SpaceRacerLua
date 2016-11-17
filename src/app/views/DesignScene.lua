10f5b02b49586e1af4c8c0f7bee5b151b099f645
sadi <272682545@qq.com>
2016-11-17 14:20:08 +0800

    增加了处理数据保存的逻辑以及移动机体的逻辑

--- src/app/views/DesignScene.lua
+++ src/app/views/DesignScene.lua
@@ -3,6 +3,36 @@ local DesignScene = class("DesignScene", cc.load("mvc").ViewBase)
 
 local TAG_ARMY_LAYER = 101
 
+local planeSet = {}
+local selectPlane = nil
+
+
+--------------function----------------
+local function convertCamera( point )
+	local camera = display.getDefaultCamera()
+	local p = camera:convertToWorldSpace(ccp(point.x, point.y))
+	p.x = p.x - display.cx
+	p.y = p.y - display.cy
+	return p
+end
+
+
+
+
+
+
+
+----------------------------
+
+
+function DesignScene:initData(  )
+	-- body
+	planeSet = {}
+
+	--点击中的机体
+	selectPlane = nil
+end
+
 function DesignScene:onCreate()
 	-- body
 	if DEBUG == 2 then 
@@ -24,6 +54,8 @@ function DesignScene:onCreate()
 	-- plane:pos(display.cx, display.cy)
 	-- plane:setCameraMask(cc.CameraFlag.DEFAULT)
 	-- self:addChild(plane)
+
+	self:initData()
 
 	self:initControl()
 
@@ -57,11 +89,31 @@ function DesignScene:initControl()
 
 	layer:onTouch(function ( event )
 		if event.name == "began" then 
-			local camera = display.getDefaultCamera()
-			local p = camera:convertToWorldSpace(ccp(event.x, event.y))
-			p.x = p.x - display.cx
-			p.y = p.y - display.cy
-			self:createPlane(p.x, p.y )
+			
+			local p = convertCamera(cc.p(event.x, event.y))
+			for c,plane in pairs(planeSet) do
+				local rect = plane:getViewRect()
+
+				local isContant = cc.rectContainsPoint(rect, p)
+				if isContant then
+					plane:setColor(cc.c3b(255, 255, 0))
+					selectPlane = plane
+					break
+				else
+					plane:setColor(cc.c3b(255, 255, 255))
+				end
+			end
+
+			if selectPlane == nil then
+				self:createPlane(p.x, p.y )
+			else
+				return true
+			end
+		elseif event.name == "moved" then 
+			local p = convertCamera(cc.p(event.x, event.y))
+			if selectPlane then 
+				selectPlane:pos(p)
+			end
 		end
 	end)
 
@@ -72,6 +124,10 @@ function DesignScene:initControl()
 			self:cameraMove(1)
 		elseif keycode == cc.KeyCode.KEY_S then 
 			self:cameraMove(-1)
+		elseif keycode == cc.KeyCode.KEY_Q then 
+			self:save()
+		elseif keycode == cc.KeyCode.KEY_SPACE then
+
 		end
 	end)
 end
@@ -81,7 +137,18 @@ function DesignScene:createPlane( x,y  )
 	local plane = PlaneFactory:getInstance():createPlane(1)
 	plane:pos(x,y)
 	layer:add(plane)
+	table.insert(planeSet, plane)
+end
 
+function DesignScene:save()
+	local tbl = {}
+	for c,plane in pairs(planeSet) do
+		local item = {}
+		item.x,item.y = plane:getPosition()
+		table.insert(tbl, item)
+	end
+
+	dump(tbl)
 end
 
 function DesignScene:cameraMove( speed )
@@ -90,7 +157,6 @@ function DesignScene:cameraMove( speed )
 end
 
 function DesignScene:onEnter()
-	print("onEnter~~~~~~~~")
 	
 end
 
